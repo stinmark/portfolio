b84e55e869dfb2753c252821f3848fc9846cd39b
Austine <austinemark254@gmail.com>
2026-07-02 19:45:46 +0300

    nfixing deploy stuck

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import './layout.css';
+	import { base } from '$app/paths'; // Import the base path helper
 	import favicon from '$lib/assets/favicon.ico';
 	import '@fontsource/chakra-petch';
+
 	let { children } = $props();
-	export const prerender = true;
 </script>
 
 <svelte:head>
@@ -14,7 +15,7 @@
 		content="Austine Mark is a product-focused software engineer building scalable, AI-powered digital systems from interface to infrastructure."
 	/>
 	<meta name="google-site-verification" content="SuKdyYLeKsPsvdvAqSG-guG4fvanUlvywxRmNxIF-tg" />
-	<link rel="icon" type="image/x-icon" href={favicon} />
+	<link rel="icon" type="image/x-icon" href="{base}/{favicon}" />
 </svelte:head>
 
 {@render children()}
